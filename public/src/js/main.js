a5fc917f6af106c321dc1bb042d880b1d5fd68ed
Paolo <Paolo>
2016-05-02 21:01:18 +0800

    added menu open, inview animations

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,3 +1,26 @@
 jQuery(document).ready(function(){
-    $('.what').show();
+    console.log('eksadasd');
+    $("#interface").waypoint(function(){
+        $(this.element).find('.moveIn').removeClass('moveIn').addClass('animated fadeInUp');
+    },{
+        offset: '50%'
+    });
+    $("#designed-for-humans").waypoint(function(){
+        $(this.element).removeClass('backgroundIn');
+    },{
+        offset: '25%'
+    });
+    $("#features").waypoint(function(){
+        $(this.element).find('.moveIn').removeClass('moveIn').addClass('animated fadeInUp');
+    },{
+        offset: '25%'
+    });
+    $(".menu-toggle").click(function(){
+        $("#bg-overlay,#main-navigation").toggleClass('off');
+        $("body").css('overflowY','hidden');
+    });
+    $("#bg-overlay").click(function(){
+        $("#bg-overlay,#main-navigation").toggleClass('off');
+        $("body").css('overflowY','scroll');
+    });
 });
